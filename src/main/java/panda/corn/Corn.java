a64873e9e4663b85df68c3f9e51cd62b0abc2ae0
cleverpanda <byrce6@gmail.com>
2017-07-17 10:17:45 -0500

    2.3.2
    server crash fix

--- src/main/java/panda/corn/Corn.java
+++ src/main/java/panda/corn/Corn.java
@@ -31,7 +31,7 @@ public class Corn {
 	
 	public static final String MODID = "simplecorn";
 	public static final String NAME = "Simple Corn";
-	public static final String VERSION = "2.3.1";
+	public static final String VERSION = "2.3.2";
 	
 	public static final boolean ISIEINSTALLED = Loader.isModLoaded("immersiveengineering");
 	
--- src/main/java/panda/corn/events/ToolTipHandler.java
+++ src/main/java/panda/corn/events/ToolTipHandler.java
@@ -2,9 +2,12 @@ package panda.corn.events;
 import net.minecraft.init.Items;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 public class ToolTipHandler {
 
 	@SubscribeEvent
+	@SideOnly(value = Side.CLIENT) 
 	public void onRenderTooltip(ItemTooltipEvent event) {
 		if(event.getItemStack().getItem() == Items.FIREWORK_CHARGE && event.getItemStack().hasTagCompound()){
 			if(event.getItemStack().getTagCompound().getCompoundTag("Explosion").getBoolean("Popcorn")){
